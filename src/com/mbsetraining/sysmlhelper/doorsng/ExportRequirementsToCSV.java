257135da73e20e1b4d4fbf8bf8325eba6a9fad7a
fjc <fraser.chadburn@mbsetraining.com>
2023-03-23 12:27:21 +0000

    #645 23-MAR-2023: ExecutableMBSE: Export requirements to csv to import into DOORS NG user interface dialog added to improve situational awareness (F.J.Chadburn) #646 23-MAR-2023: ExecutableMBSE: Get export to csv to give a more user-friendly dialog if file is locked/open in Excel (F.J.Chadburn)

--- src/com/mbsetraining/sysmlhelper/doorsng/ExportRequirementsToCSV.java
+++ src/com/mbsetraining/sysmlhelper/doorsng/ExportRequirementsToCSV.java
@@ -11,6 +11,7 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.mbsetraining.sysmlhelper.common.AnnotationMap;
+import com.mbsetraining.sysmlhelper.common.UserInterfaceHelper;
 import com.mbsetraining.sysmlhelper.executablembse.ExecutableMBSE_Context;
 import com.telelogic.rhapsody.core.*;
 
@@ -42,43 +43,48 @@ public class ExportRequirementsToCSV {
 		List<IRPRequirement> theReqts = underEl.getNestedElementsByMetaClass( 
 				"Requirement", recursive ).toList();
 
-		_context.debug( "There are " + theReqts.size() + " requirements under " + _context.elInfo( underEl ) );
-		
-		List<String> theAdditionalHeadings = new ArrayList<>();
-		
-		for( IRPRequirement theReqt : theReqts ){
+		if( theReqts.isEmpty() ) {
+			UserInterfaceHelper.showWarningDialog( "There are no requirements under " + _context.elInfo( underEl ) + " to export" );
+		} else {
+			_context.debug( "There are " + theReqts.size() + " requirements under " + _context.elInfo( underEl ) );
 			
-			AnnotationMap theAnnotationMap = new AnnotationMap( theReqt, _context );
+			List<String> theAdditionalHeadings = new ArrayList<>();
 			
-			if( !theAnnotationMap.isEmpty() ) {
+			for( IRPRequirement theReqt : theReqts ){
 				
-				String msg = _context.elInfo( theReqt ) + " has";
+				AnnotationMap theAnnotationMap = new AnnotationMap( theReqt, _context );
 				
-				for( Entry<String, List<IRPAnnotation>>  nodeId : theAnnotationMap.entrySet() ){
+				if( !theAnnotationMap.isEmpty() ) {
 					
-					String theKey = nodeId.getKey();
+					String msg = _context.elInfo( theReqt ) + " has";
 					
-					if( !theAdditionalHeadings.contains( theKey ) ){
-						theAdditionalHeadings.add( theKey );
+					for( Entry<String, List<IRPAnnotation>>  nodeId : theAnnotationMap.entrySet() ){
+						
+						String theKey = nodeId.getKey();
+						
+						if( !theAdditionalHeadings.contains( theKey ) ){
+							theAdditionalHeadings.add( theKey );
+						}
+					
+						List<IRPAnnotation> value = nodeId.getValue();
+								
+						msg += " " + nodeId.getKey() + " (" + value.size() + ")";		
 					}
-				
-					List<IRPAnnotation> value = nodeId.getValue();
-							
-					msg += " " + nodeId.getKey() + " (" + value.size() + ")";		
+					
+					_context.info( msg );
 				}
-				
-				_context.info( msg );
 			}
-		}
 
-		String theFileName = chooseAFileToImport( underEl.getName() + ".csv" );
-
-		if( theFileName != null ){
-
-			_context.info( "User chose " + theFileName + " to export " + theReqts.size() + " requirements" );
-
-			try {
-
+			String theFilename = underEl.getName() + ".csv";
+			File theFile = chooseAFileToImport( theFilename );
+			
+			if( theFile == null ) {
+				
+				UserInterfaceHelper.showWarningDialog( 
+						"No file was selected to export requirement to" );
+					
+			} else {
+				
 				// Get controlling properties
 				String artifactTypeForCSVExport = _context.getCSVExportArtifactType( underEl );
 				String separator = _context.getCSVExportSeparator( underEl );
@@ -88,88 +94,139 @@ public class ExportRequirementsToCSV {
 						", CSVExportArtifactType=" + artifactTypeForCSVExport + 
 						", CVSExportIncludeArtifactName=" + isNameForCVSExport );
 
-				FileWriter myWriter = new FileWriter( theFileName );
-
-				if( !separator.equals("," ) ){
-					String info = "sep=" + separator;
-					_context.info( "Added " + info + " to export as CSVExportSeparator property is not a comma" );
-					myWriter.write( info + "\n" );
-				}
+				String theMsg = "Based on the ExecutableMBSEProfile::RequirementsAnalysis properties for this package this helper will export " + theReqts.size();
 				
-				String theHeadingLine = "";
-				
-				if( isNameForCVSExport ){
-					theHeadingLine = "Artifact Type" + separator + "Name" + separator + "Primary Text";
+				if( theReqts.size() == 1 ) {
+					theMsg += " requirement.";
 				} else {
-					theHeadingLine = "Artifact Type" + separator + "Primary Text";
+					theMsg += " requirements.";
 				}
 				
-				for( String theAdditionalHeading : theAdditionalHeadings ){
-					theHeadingLine += separator + theAdditionalHeading;
+				theMsg += "\n\nSource: " + _context.elInfo( underEl ) + 
+						" \n" + "Target: " + theFile.getAbsolutePath() + "\n" +
+						"Artifact Type: " + artifactTypeForCSVExport + "\n";
+				
+				if( isNameForCVSExport ) {
+					theMsg += "Name column: True";
+				} else {
+					theMsg += "Name column: False";
 				}
 				
-				theHeadingLine += "\n";
-						
-				myWriter.write( theHeadingLine );
-
-				for( IRPRequirement theReqt : theReqts ){
-
-					String theLine = "";
-					String theName = theReqt.getName();
+				theMsg += "\n\nDo you want to proceed? ";
+				
+				boolean answer = UserInterfaceHelper.askAQuestion( theMsg );
+				
+				if( answer ) {
 					
-					String theSpecification = _context.
-							removeCSVIncompatibleCharsFrom( theReqt.getSpecification() );
+					_context.info( theFile.getAbsolutePath() + " (Target File)");
 
-					if( isNameForCVSExport ){
-						theLine = artifactTypeForCSVExport + separator + theName + separator + theSpecification;
-					} else {
-						theLine = artifactTypeForCSVExport + separator + theSpecification;
-					}
-					
-					AnnotationMap theAnnotationMap = new AnnotationMap( theReqt, _context );
+					try {
 
-					for( String theAdditionalHeading : theAdditionalHeadings ){
+						boolean isContinue = true;
 						
-						theLine += separator;
+						FileWriter myWriter = null;
 						
-						List<IRPAnnotation> theSpecificAnnotations = theAnnotationMap.
-								getOrDefault( theAdditionalHeading, new ArrayList<>() );
-						
-						Iterator<IRPAnnotation> iterator = theSpecificAnnotations.iterator();
-						
-						while( iterator.hasNext() ){
+						try {
 							
-							IRPAnnotation theSpecificAnnotation = (IRPAnnotation) iterator.next();
+							myWriter = new FileWriter( theFile );
+
+							String info = "sep=" + separator;
+							_context.info( "Added " + info + " to export as CSVExportSeparator property" );
 							
-							String theDescription = _context.
-									removeCSVIncompatibleCharsFrom( theSpecificAnnotation.getDescription() );
+							myWriter.write( info + "\n" );
+
+						} catch( Exception e ) {
 							
-							if( theSpecificAnnotations.size() <= 1 ) {
-								theLine += theDescription;
+							isContinue = false;
+							
+							UserInterfaceHelper.showWarningDialog( 
+									"Unable to export requirements to " + theFile.getAbsolutePath() + " \n" +
+									"File is not writable. If it's open in another program, close it and try again." );						
+						}
+						
+						if( isContinue ) {
+							
+							String theHeadingLine = "";
+							
+							if( isNameForCVSExport ){
+								theHeadingLine = "Artifact Type" + separator + "Name" + separator + "Primary Text";
 							} else {
-								theLine += theDescription + "(" + theSpecificAnnotation.getName() + ")";	
+								theHeadingLine = "Artifact Type" + separator + "Primary Text";
 							}
-						}		
+							
+							for( String theAdditionalHeading : theAdditionalHeadings ){
+								theHeadingLine += separator + theAdditionalHeading;
+							}
+							
+							theHeadingLine += "\n";
+									
+							myWriter.write( theHeadingLine );
+
+							for( IRPRequirement theReqt : theReqts ){
+
+								String theLine = "";
+								String theName = theReqt.getName();
+								
+								String theSpecification = _context.
+										removeCSVIncompatibleCharsFrom( theReqt.getSpecification() );
+
+								if( isNameForCVSExport ){
+									theLine = artifactTypeForCSVExport + separator + theName + separator + theSpecification;
+								} else {
+									theLine = artifactTypeForCSVExport + separator + theSpecification;
+								}
+								
+								AnnotationMap theAnnotationMap = new AnnotationMap( theReqt, _context );
+
+								for( String theAdditionalHeading : theAdditionalHeadings ){
+									
+									theLine += separator;
+									
+									List<IRPAnnotation> theSpecificAnnotations = theAnnotationMap.
+											getOrDefault( theAdditionalHeading, new ArrayList<>() );
+									
+									Iterator<IRPAnnotation> iterator = theSpecificAnnotations.iterator();
+									
+									while( iterator.hasNext() ){
+										
+										IRPAnnotation theSpecificAnnotation = (IRPAnnotation) iterator.next();
+										
+										String theDescription = _context.
+												removeCSVIncompatibleCharsFrom( theSpecificAnnotation.getDescription() );
+										
+										if( theSpecificAnnotations.size() <= 1 ) {
+											theLine += theDescription;
+										} else {
+											theLine += theDescription + "(" + theSpecificAnnotation.getName() + ")";	
+										}
+									}		
+								}
+								
+								theLine += "\n";
+								
+								myWriter.write( theLine );
+							}
+							
+							myWriter.close();
+
+						}
+
+
+					} catch( Exception e ){
+
+						_context.error( "Exception, e=" + e.getMessage() );
+						System.out.println( "An error occurred." );
+						e.printStackTrace();
 					}
-					
-					theLine += "\n";
-					
-					myWriter.write( theLine );
 				}
-
-				myWriter.close();
-
-			} catch( Exception e ){
-
-				_context.error( "Exception, e=" + e.getMessage() );
-				System.out.println( "An error occurred." );
-				e.printStackTrace();
 			}
 		}
 	}
 
-	private String chooseAFileToImport(
+	private File chooseAFileToImport(
 			String theFilename ){
+
+		File theFile = null;
 
 		JFileChooser fc = new JFileChooser();
 
@@ -202,12 +259,14 @@ public class ExportRequirementsToCSV {
 				if (!theFilename.endsWith(".csv")){
 					theFilename = theFilename + ".csv";
 				}
+
+				theFile = new File( theFilename );	
 			}
 		} else {
 			_context.debug( "User chose to cancel" );
 		}
 
-		return theFilename;
+		return theFile;
 	}
 
 	public class CSVFileFilter extends javax.swing.filechooser.FileFilter {
